2489da609ad6cf9a3995b5bf9d7898c43d87fd91
ladyangel1981 <51683643+ladyangel1981@users.noreply.github.com>
2020-12-10 00:39:40 +0100

    Update Connection_BBDD.py

---
+++ Connection_BBDD.py
@@ -0,0 +1,39 @@
+"""
+import sys
+import boto3
+
+ENDPOINT = "sieteymedio.cif7u0be4daj.us-east-1.rds.amazonaws.com"
+PORT = "3306"
+USR = "root"
+REGION = "us-east-1"
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+
+# gets the credentials from .aws/credentials
+session = boto3.Session(profile_name='RDSCreds')
+client = boto3.client('rds')
+
+token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USR, Region=REGION)
+"""
+
+import pymysql
+
+# import pandas as pd
+# from sqlalchemy import create_engine
+
+# Configuramos la BBDD
+
+# Conexión de base de datos
+host = "sieteymedio.cif7u0be4daj.us-east-1.rds.amazonaws.com"
+port = 3306
+# Usuario de la conexión
+user = "root"
+# Contraseña
+passwd = "Alumneroot"
+# Nombre de la base de datos a la cual nos vamos a conectar
+BBDD = "sieteymedio"
+
+db = pymysql.connect(host, port, user, passwd, BBDD)
+# db = create_engine('mysql+pymysql://' + user + ':' + passwd + '@' + host + ':' + str(port) + '/' + BBDD, echo=False)
+
+# Este cursor lo usamos para ejecutar la query y almacenar sus datos
+cursor = db.cursor()
