74b26ab775e8dc81e6bb50e2ae7402f8e606da66
ladyangel1981 <51683643+ladyangel1981@users.noreply.github.com>
2020-12-12 01:12:22 +0100

    UPDATED

--- Connection_BBDD.py
+++ Connection_BBDD.py
@@ -23,16 +23,15 @@ import pymysql
 # Configuramos la BBDD
 
 # Conexión de base de datos
-host = "sieteymedio.cif7u0be4daj.us-east-1.rds.amazonaws.com"
-port = 3306
+host = "localhost"
 # Usuario de la conexión
 user = "root"
 # Contraseña
-passwd = "Alumneroot"
+passwd = "root"
 # Nombre de la base de datos a la cual nos vamos a conectar
-BBDD = "sieteymedio"
+BBDD = "proyecto"
 
-db = pymysql.connect(host, port, user, passwd, BBDD)
+db = pymysql.connect(host, user, passwd, BBDD)
 # db = create_engine('mysql+pymysql://' + user + ':' + passwd + '@' + host + ':' + str(port) + '/' + BBDD, echo=False)
 
 # Este cursor lo usamos para ejecutar la query y almacenar sus datos
