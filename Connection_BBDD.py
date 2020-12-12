0f7c5f7217bf48da514beb8c506eefe9ebc2653f
ladyangel1981 <51683643+ladyangel1981@users.noreply.github.com>
2020-12-12 21:15:38 +0100

    updated

--- Connection_BBDD.py
+++ Connection_BBDD.py
@@ -1,26 +1,7 @@
-"""
-import sys
-import boto3
-
-ENDPOINT = "sieteymedio.cif7u0be4daj.us-east-1.rds.amazonaws.com"
-PORT = "3306"
-USR = "root"
-REGION = "us-east-1"
-os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
-
-# gets the credentials from .aws/credentials
-session = boto3.Session(profile_name='RDSCreds')
-client = boto3.client('rds')
-
-token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USR, Region=REGION)
-"""
-
+# IMPORTS
 import pymysql
 
-# import pandas as pd
-# from sqlalchemy import create_engine
-
-# Configuramos la BBDD
+# CONFIGURAMOS LA BBDD
 
 # Conexión de base de datos
 host = "localhost"
@@ -32,7 +13,16 @@ passwd = "root"
 BBDD = "proyecto"
 
 db = pymysql.connect(host, user, passwd, BBDD)
-# db = create_engine('mysql+pymysql://' + user + ':' + passwd + '@' + host + ':' + str(port) + '/' + BBDD, echo=False)
 
 # Este cursor lo usamos para ejecutar la query y almacenar sus datos
 cursor = db.cursor()
+
+# ejecuta el SQL query usando el metodo execute().
+cursor.execute("SELECT VERSION()")
+
+# procesa una unica linea usando el metodo fetchone().
+data = cursor.fetchone()
+print("Database version : {0}".format(data))
+
+# desconecta del servidor
+db.close()
